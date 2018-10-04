3b043f6258e48b1e717c360484c6945465ead0e5
artinZareie <35101640+artinZareie@users.noreply.github.com>
2018-10-04 06:05:42 -0700

    Update Pyc.h

--- Pyc.h
+++ Pyc.h
@@ -4,75 +4,17 @@
 
 using namespace std;
 
-inline void print(string experession) {
-	cout << experession;
-}
-
-inline void print(char experession) {
-	cout << experession;
-}
-
-inline void print(int experession) {
-	cout << experession;
-}
-
-inline void print(float experession) {
-	cout << experession;
-}
-
-inline void print(double experession) {
-	cout << experession;
-}
-
-inline void print(long experession) {
-	cout << experession;
-}
-
-inline void print(unsigned int experession) {
-	cout << experession;
-}
-
-inline void print(unsigned long experession) {
+template < class T > inline void print(T experession) {
 	cout << experession;
 }
 
 
-
-inline void print_line(string experession) {
-	cout << '\n' << experession << '\n';
-}
-
-inline void print_line(char experession) {
-	cout << '\n' << experession << '\n';
-}
-
-inline void print_line(int experession) {
-	cout << '\n' << experession << '\n';
-}
-
-inline void print_line(float experession) {
-	cout << '\n' << experession << '\n';
-}
-
-inline void print_line(double experession) {
-	cout << '\n' << experession << '\n';
-}
-
-inline void print_line(long experession) {
+template < class T >inline void print_line(T experession) {
 	cout << '\n' << experession << '\n';
 }
 
 
-inline void print_line(unsigned int experession) {
-	cout << '\n' << experession << '\n';
-}
-
-inline void print_line(unsigned long experession) {
-	cout << '\n' << experession << '\n';
-}
-
-
-inline auto input(string prompt = "") {
+template < class T > inline T input(string prompt = "") {
 	auto data = false;
 	cout << prompt;
 	cin >> data;
@@ -80,49 +22,7 @@ inline auto input(string prompt = "") {
 	return data;
 }
 
-inline void input(string &refrence, string prompt = "") {
-	cout << prompt;
-	cin >> refrence;
-	cout << endl;
-}
-
-inline void input(int &refrence, string prompt = "") {
-	cout << prompt;
-	cin >> refrence;
-	cout << endl;
-}
-
-inline void input(unsigned int &refrence, string prompt = "") {
-	cout << prompt;
-	cin >> refrence;
-	cout << endl;
-}
-
-inline void input(unsigned long &refrence, string prompt = "") {
-	cout << prompt;
-	cin >> refrence;
-	cout << endl;
-}
-
-inline void input(float &refrence, string prompt = "") {
-	cout << prompt;
-	cin >> refrence;
-	cout << endl;
-}
-
-inline void input(double &refrence, string prompt = "") {
-	cout << prompt;
-	cin >> refrence;
-	cout << endl;
-}
-
-inline void input(bool &refrence, string prompt = "") {
-	cout << prompt;
-	cin >> refrence;
-	cout << endl;
-}
-
-inline void input(char &refrence, string prompt = "") {
+template < class T > inline void input(T &refrence, string prompt = "") {
 	cout << prompt;
 	cin >> refrence;
 	cout << endl;
